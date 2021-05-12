7a4c7027a4e321c1c11a9c6246ed91695e8d912e
edwin4v@gmail.com <edwin4v@gmail.com>
2021-05-12 16:42:45 +0800

    Added deployment code

--- deployment/app.py
+++ deployment/app.py
@@ -6,6 +6,11 @@ from skimage.color import rgb2lab, lab2rgb
 app = FastAPI()
 turbojpeg = TurboJPEG()
 
+import onnxruntime
+
+sess_options = onnxruntime.SessionOptions()
+sess = onnxruntime.InferenceSession('model/color_gen.onnx', sess_options)
+
 @app.post('/')
 async def get_index(file: UploadFile = File(...)):
     contents = await file.read()
@@ -13,8 +18,25 @@ async def get_index(file: UploadFile = File(...)):
     img = turbojpeg.decode(img_buffer)
 
     lab_img = rgb2lab(img).astype(np.float32)
-    L = lab_img[[0], ...] / 50. - 1. # Between -1 and 1
+    L = lab_img[:,:, 0] / 50. - 1.
+    L = np.expand_dims(L, axis=0)
+    L = np.expand_dims(L, axis=-1)
+    
+    pred_ab = sess.run([output_name], {
+        input_name: np.transpose(L, (0, 3, 1, 2))
+    })[0]
+    
+    
+    # Post processing
+    pred_ab = np.squeeze(pred_ab, axis=0) * 110
+
+    pred_ab = np.transpose(pred_ab, (1, 2, 0))
+
+    empty_img = np.zeros((256, 256, 3))
+    empty_img[:, :, 0] = lab_img[:, :, 0]
+    empty_img[:, :, 1:] = pred_ab
+    rgb_pred = lab2rgb(empty_img)
     
     return {
-        'img': str(img_buffer)
+        'img': turbojpeg.encode(rgb_pred).decode('ISO-8859-1')
     }
