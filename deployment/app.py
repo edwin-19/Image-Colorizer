f949b58fd6e0ace103104d58247dee2b0580d831
edwin4v@gmail.com <edwin4v@gmail.com>
2021-05-12 16:11:07 +0800

    added onnx inference to pix2pix model

--- deployment/app.py
+++ deployment/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
 from turbojpeg import TurboJPEG
+from skimage.color import rgb2lab, lab2rgb
 
 app = FastAPI()
 turbojpeg = TurboJPEG()
@@ -10,6 +11,9 @@ async def get_index(file: UploadFile = File(...)):
     contents = await file.read()
     img_buffer = np.frombuffer(contents, dtype=np.uint8)
     img = turbojpeg.decode(img_buffer)
+
+    lab_img = rgb2lab(img).astype(np.float32)
+    L = lab_img[[0], ...] / 50. - 1. # Between -1 and 1
     
     return {
         'img': str(img_buffer)
---
+++ pix2pix/README.md
@@ -0,0 +1,7 @@
+# Pix2pix model for color conversion
+- Same principle as the autoencoder version but with the added discriminatior to help differentiate
+
+# Inituition - How it works
+
+# Dynamic Unet()
+- Using a pretrain
